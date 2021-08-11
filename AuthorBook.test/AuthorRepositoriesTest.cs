deb164db8b39ad33d64c54b1fd27609ce3ab32e8
Nicolaj Schønberg <48961648+Tristan1449@users.noreply.github.com>
2021-08-11 19:19:57 +0200

    AuthorRepTEST

--- AuthorBook.test/AuthorRepositoriesTest.cs
+++ AuthorBook.test/AuthorRepositoriesTest.cs
@@ -1,6 +1,7 @@
 ﻿using AuthorBook.Controllers;
 using AuthorBook.domain;
 using AuthorBook.repositori;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Moq;
 using System.Collections.Generic;
@@ -12,33 +13,21 @@ namespace AuthorBook.test
     public class AuthorRepositoriesTest
     {
         [Fact]    
-        public async void getAll_something()
+        public async void GetAuthor_Return200()
         {
             //tripel a
             //arrange - opsætning
             //objController and mok data
-            var dataSource = new Mock<IAuthorRepositori>();
+            Mock<IAuthorRepositori> dataSource = new Mock<IAuthorRepositori>();
             List<author> authorList = new List<author>();
-            authorList.Add(new author
-            {
-                Id = 10,
-                Firstname ="John",
-                Lastname="Kurt"
-            });
-            authorList.Add(new author 
-            {
-                Id = 11,
-                Firstname = "Kurt",
-                Lastname = "John"
-            });
             dataSource.Setup(s => s.getAuthors()).ReturnsAsync(authorList);
 
             //act - handling
             Author1controller classThatIsTested = new Author1controller(dataSource.Object);
-            var result = await classThatIsTested.getAuthors();
+            IActionResult result = await classThatIsTested.getAuthors();
 
             //assert - verificer
-            var StatusCodeResult = (IStatusCodeActionResult)result;
+            IStatusCodeActionResult StatusCodeResult = (IStatusCodeActionResult)result;
             Assert.Equal(200, StatusCodeResult.StatusCode);
             
         }
